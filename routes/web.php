3254cdeb11ac101c8a765dc86007af1af607ac09
ahmad hakim <113881962+aimgov@users.noreply.github.com>
2024-12-21 08:49:35 +0700

    subscribe plan page

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\SubscribeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/subcription/plans', function () {
-    return 'Subscription plans';
-})->name('subscription.plans');
+Route::get('/subcription/plans', [SubscribeController::class, 'showPlans'])->name('subscription.plans');
