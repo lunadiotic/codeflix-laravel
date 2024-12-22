494f92ecf095da843ce16f31b2b2788e2b1bcb0e
ahmad hakim <ahmad.hakim@wartek.belajar.id>
2024-12-22 23:21:47 +0700

    showing movies on homepage

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Movie;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
 
-class MovieController extends Controller
+class MovieController extends Controller implements HasMiddleware
 {
+
+    /**
+     * Get the middleware that should be assigned to the controller.
+     */
+    public static function middleware(): array
+    {
+        return [
+            'auth',
+            'check.device'
+        ];
+    }
+
     public function index()
     {
-        return view('movies.index');
+        $latestMovies = Movie::latest()->limit(8)->get();
+        $topRatedMovies = Movie::with('ratings')
+            ->get()
+            ->sortByDesc('average_rating')
+            ->take(8);
+        return view('movies.index', [
+            'latestMovies' => $latestMovies,
+            'topRatedMovies' => $topRatedMovies
+        ]);
     }
 }
